c2e30ae1c66a0838edfda52da770b20b3eedae01
Iwamgad <dagmawiharry2@gmail.com>
2022-11-21 19:59:49 +0000

    I did task 3 again

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,22 +1,16 @@
 #!/usr/bin/python3
-"""app.py to connect to API"""
-import os
+"""This module initiates the api"""
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
-from flask import Flask, Blueprint, jsonify, make_response
+from os import getenv
 from flask_cors import CORS
 
-
-app = Flask('app')
-# register the blueprint app_views to your Flask instance app
+app = Flask(__name__)
+app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
-cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
-
-@app.teardown_appcontext
-def teardown_appcontext(code):
-    """teardown_appcontext"""
-    storage.close()
+cors = CORS(app, resources={r'/*': {'origins': '0.0.0.0'}})
 
 
 @app.errorhandler(404)
@@ -24,9 +18,24 @@ def page_not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 
+@app.errorhandler(400)
+def bad_request(error):
+    d = error.description
+    msgs = ["Missing name", "Missing email",
+            "Missing password", "Missing user_id",
+            "Missing text"]
+    message = 'Not a Json' if d not in msgs else d
+    return make_response(jsonify({'error': message}), 400)
+
+
+@app.teardown_appcontext
+def tear_down_db(execute):
+    """Removes the current SQLAlchemy session after each request
+    is completed"""
+    storage.close()
+
+
 if __name__ == "__main__":
-    # run your Flask server (variable app) with:
-    # host = environment variable HBNB_API_HOST or 0.0.0.0 as default value
-    # port = environment variable HBNB_API_PORT or 5000 as default value
-    app.run(host=os.getenv('HBNH_API_HOST', '0.0.0.0'),
-            port=int(os.getenv('HBNB_API_PORT', 5000)))
+    app.run(host=getenv('HBNB_API_HOST', '0.0.0.0'),
+            port=getenv('HBNB_API_PORT', 5000),
+            threaded=True, debug=True)
