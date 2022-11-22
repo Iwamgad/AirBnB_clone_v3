cd167997d207643340c7f73f2c44a33fad7de619
Iwamgad <dagmawiharry2@gmail.com>
2022-11-22 14:56:10 +0000

    I edited task 4 again

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,33 +1,37 @@
 #!/usr/bin/python3
-"""index.py to connect to API"""
+"""Python file that routes the Flask API's"""
 from api.v1.views import app_views
-from flask import Flask, Blueprint, jsonify
+from flask import jsonify, request
 from models import storage
 
-hbnbText = {
-    "amenities": "Amenity",
-    "cities": "City",
-    "places": "Place",
-    "reviews": "Review",
-    "states": "State",
-    "users": "User"
-}
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.user import User
+from models.state import State
 
 
-@app_views.route('/status', strict_slashes=False)
-def hbnbStatus():
-    """hbnbStatus"""
+@app_views.route('/status', methods=['GET'])
+def show_status():
+    """An end point to retrive ok status as a response"""
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats', strict_slashes=False)
-def hbnbStats():
-    """hbnbStats"""
-    return_dict = {}
-    for key, value in hbnbText.items():
-        return_dict[key] = storage.count(value)
-    return jsonify(return_dict)
+@app_views.route('/stats', methods=['GET'])
+def stats():
+    """An endpoint to retrive the number of each objects by type"""
+    mapper = {
+            "amenities": Amenity,
+            "cities": City,
+            "places": Place,
+            "reviews": Review,
+            "states": State,
+            "users": User
+            }
 
+    result = {}
+    for k, v in mapper.items():
+        result[k] = storage.count(v)
 
-if __name__ == "__main__":
-    pass
+    return jsonify(result)
